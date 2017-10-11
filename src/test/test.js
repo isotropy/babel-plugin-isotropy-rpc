557346681452365ba0d7bb9b68aad01bf83d41fd
arivanandan <uarivanandan@gmail.com>
2017-10-11 17:51:26 +0530

    Returns visitor instead of plugin

--- src/test/test.js
+++ src/test/test.js
@@ -2,13 +2,39 @@ import should from "should";
 import * as babel from "babel-core";
 import fs from "fs";
 import path from "path";
-import makePlugin from "../transform-to-isotropy-rpc";
+import transformToIsotropyWebservices from "../transform-to-isotropy-webservices";
 import sourceMapSupport from "source-map-support";
 
 sourceMapSupport.install();
 
 describe("isotropy-ast-analyzer-webservices", () => {
-  function run([description, dir, opts]) {
+  function run([description, dir]) {
+    const opts = {
+      plugins: [
+        [
+          transformToIsotropyWebservices(),
+          {
+            projects: [
+              {
+                dir: "dist/test/fixtures",
+                modules: [
+                  {
+                    source: "my-server",
+                    url: "https://www.poe3.com"
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        "transform-object-rest-spread"
+      ],
+      parserOpts: {
+        sourceType: "module",
+        allowImportExportEverywhere: true
+      },
+      babelrc: false
+    };
     it(`${description}`, () => {
       const fixturePath = path.resolve(
         __dirname,
@@ -16,51 +42,19 @@ describe("isotropy-ast-analyzer-webservices", () => {
         dir,
         `fixture.js`
       );
-      const outputPath = path.resolve(__dirname, "fixtures", dir, `output.js`);
       const expected = fs
         .readFileSync(__dirname + `/fixtures/${dir}/expected.js`)
         .toString();
-      const pluginInfo = makePlugin(opts);
 
-      const babelResult = babel.transformFileSync(fixturePath, {
-        plugins: [
-          [
-            pluginInfo.plugin,
-            {
-              projects: [
-                {
-                  dir: "dist/test/fixtures",
-                  modules: [
-                    {
-                      source: "my-server",
-                      url: "https://www.poe3.com"
-                    }
-                  ]
-                }
-              ]
-            }
-          ],
-          "transform-object-rest-spread"
-        ],
-        parserOpts: {
-          sourceType: "module",
-          allowImportExportEverywhere: true
-        },
-        babelrc: false
-      });
+      const babelResult = babel.transformFileSync(fixturePath, opts);
       const actual = babelResult.code + "\n";
       actual.should.deepEqual(expected);
     });
   }
 
-  const tests = [
+  [
     ["rpc", "rpc"],
     ["rpc-args", "rpc-args"],
     ["rpc-deep", "rpc-deep"]
-  ];
-
-  for (const test of tests) {
-    debugger;
-    run(test);
-  }
+  ].forEach(test => run(test));
 });
